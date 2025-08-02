86b9409b63a0f17960e7d6ca0be771162564f7f5
Hamidreza Jahandideh <Hamidreza.jahandideh1994@gmail.com>
2025-08-02 12:36:20 +0330

    bug fixed

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.qb5fndqlkk8"
+    "revision": "0.2kcvodk1f4g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
