f273d24a695689eb38173a068d3efdb00366430a
roغئخت <roymoj06@gmail.com>
2025-10-25 13:42:20 +0330

    fix: resolve tsconfig.app.json glob patterns and add build:dev script

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.v62ibd1bvpo"
+    "revision": "0.jdg4a2cosao"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
